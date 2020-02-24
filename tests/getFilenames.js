6615e7a6ea29b922c9ebf80c609d8195cd5c6c68
Artem <artem@framer.com>
2020-02-24 14:21:17 +0100

    better tests

--- tests/getFilenames.js
+++ tests/getFilenames.js
@@ -1,19 +1,24 @@
-const { getFilenames } = require('../src/index.js');
-const root = __dirname + '/test-dir';
+const assert = require("assert");
+const { getFilenames } = require("../src/index.js");
+const root = __dirname + "/test-dir";
 const promises = [
   getFilenames(root, /\.js$/i),
-  getFilenames(root, '.js'),
-  getFilenames(root, (f) => ~f.indexOf('.js'))
+  getFilenames(root, ".js"),
+  getFilenames(root, f => ~f.indexOf(".js"))
 ];
 
-Promise.all(promises).then((results) => {
-  console.assert(results[0].files[0] === results[1].files[0]);
-  console.assert(results[0].files[0] === results[1].files[0]);
-  console.assert(results[0].files.length === 3);
-  console.assert(results[0].files.length === results[1].files.length);
-  console.assert(results[0].files.length === results[1].files.length);
-  return '';
-}, err => {
-  console.log('=========================err==========================');
-  console.dir(err);
-}).then(console.log.bind(console, 'getFilenames: DONE'));
+Promise.all(promises)
+  .then(results => {
+    assert(results[0].files[0] === results[1].files[0]);
+    assert(results[0].files.length === 3);
+    assert(results[0].files.length === results[1].files.length);
+    assert(results[0].files.length === results[1].files.length);
+    return "";
+  })
+  .then(
+    console.log.bind(console, "\x1b[32m%s\x1b[0m%s", "DONE: ", "getFilenames")
+  )
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
--- tests/processFiles.js
+++ tests/processFiles.js
@@ -1,31 +1,38 @@
-const { getFilenames, processFiles } = require('../src/index.js');
-const root = __dirname + '/test-dir';
+const assert = require("assert");
+const { getFilenames, processFiles } = require("../src/index.js");
+const root = __dirname + "/test-dir";
 const processed = [];
 
-getFilenames(root, '.js').then(({ files }) => {
-  return processFiles(files, (buffer, filename) => {
-    console.assert(~files.indexOf(filename), 'filename is not in files list');
-    return new Promise((resolve, reject) => {
-      setTimeout(()=> {
-        processed.push(filename);
-        if (~filename.indexOf('2')) {
-          reject('eg. Currupted file');
-          return;
-        }
-        resolve();
-      }, 100);
+getFilenames(root, ".js")
+  .then(({ files }) => {
+    return processFiles(files, (buffer, filename) => {
+      assert(~files.indexOf(filename), "filename is not in files list");
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          processed.push(filename);
+          if (~filename.indexOf("2")) {
+            reject("eg. Currupted file");
+            return;
+          }
+          resolve();
+        }, 100);
+      });
     });
+  })
+  .then(({ results, errors }) => {
+    assert(
+      processed.length === 3,
+      "processed.length is incorrect",
+      processed.length
+    );
+    assert(
+      Object.keys(errors).length === 1,
+      "processed.length is incorrect",
+      processed.length
+    );
+    console.log("\x1b[32m%s\x1b[0m%s", "DONE: ", "processFiles");
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
   });
-}, err => {
-  console.log('=========================err==========================');
-  console.dir(err);
-})
-.then(({results, errors}) => {
-  console.log('=========================res==========================');
-  console.dir(results);
-  console.log('=========================errors==========================');
-  console.dir(errors);
-  console.assert(processed.length === 3, 'processed.length is incorrect', processed.length);
-  console.assert(Object.keys(errors).length === 1, 'processed.length is incorrect', processed.length);
-  console.log('processFiles: DONE');
-});
